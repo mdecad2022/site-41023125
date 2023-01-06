bfb80b66d190a8c0c6645286eea8ec80b7630ff0
41023125 <41023125@gm.nfu.edu.tw>
2023-01-06 09:56:16 +0000

    w6 NX

---
+++ downloads/ipv6/cad.bat
@@ -0,0 +1,6 @@
+@echo off
+start  https://exam.cycu.org/
+start  https://stud.cycu.org:8150/
+start  E:\FileZillaPortable\FileZillaPortable.exe
+start  /MIN cmd.exe /k ssh cad41023125@stud.cycu.org 
+start https://microsoft.com/
